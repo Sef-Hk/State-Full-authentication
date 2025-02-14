4a42b6c617a842505507b5fd97383b22069ebd7d
Sef-Hk <ahk.sefsaf@gmail.com>
2025-02-13 18:23:21 -0800

    t

--- frontend/Auth-Sys/src/components/Form.jsx
+++ frontend/Auth-Sys/src/components/Form.jsx
@@ -14,7 +14,7 @@ function Form() {
       const navigate = useNavigate();
       const [skillsError, setSkillsError] = useState(false);
       const [roleError, setRoleError] = useState(false);
-      const API_URL = process.env.REACT_APP_API_URL
+      const API_URL = process.env.BackEnd_URL
       const [ageError, setAgeError] = useState("");
       const [formData, setFormData] = useState({
             role:"",
