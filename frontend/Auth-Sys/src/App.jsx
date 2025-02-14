981dc8277ad9ced1f5086e7f25921b6c94652682
Sef-Hk <ahk.sefsaf@gmail.com>
2025-02-13 22:48:41 -0800

    s

--- frontend/Auth-Sys/src/App.jsx
+++ frontend/Auth-Sys/src/App.jsx
@@ -3,7 +3,6 @@ import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
 import SignUp from './components/SignUp'
 import Login from './components/Login'
 import Home from './components/Home'
-
 import './App.css'
 function App() {
   return (
